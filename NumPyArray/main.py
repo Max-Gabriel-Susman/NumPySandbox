e195d43d950ac7e379e5417625e091bd5e738a34
Max Gabriel Susman <gabe.susman@gmail.com>
2025-06-18 01:00:30 -0600

    included an example of numpy array operations

--- NumPyArray/main.py
+++ NumPyArray/main.py
@@ -2,3 +2,7 @@ import numpy as np
 
 test_1 = np.array([92, 94, 88, 91, 87])
 test_2 = np.genfromtxt('test_2.csv', delimiter=',')
+
+# array operations 
+test_3 = np.array([87, 85, 72, 90, 92])
+test_3_fixed = test_3 + 2
