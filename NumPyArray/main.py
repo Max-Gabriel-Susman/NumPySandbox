49316686dffe15731f35f94702822d4770eeef59
Max Gabriel Susman <gabe.susman@gmail.com>
2025-06-20 13:07:34 -0600

    included an example of numpy ops across multiple arrays

--- NumPyArray/main.py
+++ NumPyArray/main.py
@@ -6,3 +6,8 @@ test_2 = np.genfromtxt('test_2.csv', delimiter=',')
 # array operations 
 test_3 = np.array([87, 85, 72, 90, 92])
 test_3_fixed = test_3 + 2
+
+# multiple numpy array ops
+total_grade = test_1 + test_2 + test_3_fixed
+final_grade = total_grade / 3
+print(final_grade)
