56628af08b99c4a2171147849bfc7eb031b8c110
Max Gabriel Susman <gabe.susman@gmail.com>
2025-06-23 00:27:42 -0600

    selecting elements from multi-dimensional arrays

--- NumPyArray/main.py
+++ NumPyArray/main.py
@@ -20,3 +20,12 @@ manual_adwoa_test_1 = np.array([test_1[1], test_1[2]])
 coin_toss = np.array([1, 0, 0, 1, 0])
 
 coin_toss_again = np.array([[1, 0, 0, 1, 0], [0, 0, 1, 1, 1]])
+
+# selecting elements from multi-dimensional arrays 
+student_scores = np.array([[92, 94, 88, 91, 87],
+                           [79, 100, 86, 93, 91],
+                           [87, 85, 72, 90, 92]])
+
+tanya_test_3 = student_scores[2, 0]
+
+cody_test_scores = student_scores[:,4]
