36cfcb83ab4597aac41f74b9bfb17828644de50f
Max Gabriel Susman <gabe.susman@gmail.com>
2025-07-05 03:09:25 -0600

    included a simple calculation of the mean

---
+++ NumPyMean/calculating_mean.py
@@ -0,0 +1,9 @@
+# Set total equal to the sum
+total = 29 + 49 + 42 + 43
+
+# Set mean_value equal to the mean
+mean_value = total / 4
+
+# The following code prints the total and mean
+print("The sum total is equal to: " + str(total))
+print("The mean value is equal to: " + str(mean_value))
